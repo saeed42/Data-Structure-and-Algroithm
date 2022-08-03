2ee71777a37928eb7ef1019ecc1adfb0ded4336c
saeed42 <42887083+saeed42@users.noreply.github.com>
2022-08-03 12:02:24 +0600

    Create Array.c
    It is array file

---
+++ Array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, n, num;
+  
+  scanf("%d", &num);
+  
+  int arr[num];
+  
+  //input array element
+  for(i = 0; i < num; i++) {
+      scanf("%d", &a[i]);
+  }
+  
+  
+  //print array element
+  
+  for(i = 0; i < num; i++)  {
+      printf("%d\n", a[i])
+  }
+  
+  return 0;
+  
+}
